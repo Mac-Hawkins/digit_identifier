e8b90e7f0287ec19efc19992a5ddb27f6e727fe9
Mac-Hawkins <mckenziejhawkins8@gmail.com>
2026-05-19 18:11:17 -0400

    Created the code for the backend server.
    - Moved models into backend.
    - Added .gitignore.

---
+++ web_app/backend/main.py
@@ -0,0 +1,57 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import tensorflow as tf
+
+# Constants
+IMAGE_HEIGHT = 28
+IMAGE_WIDTH = 28
+IMAGE_CHANNELS = 1
+MAX_PIXEL_VALUE = 255.0
+
+# Class to represent pixel data from the frontend.
+class Image(BaseModel):
+    data: str  # base64 string from the frontend. Each pixel value will be represented by it's corresponding char.
+
+# Load the pre-trained model in.
+model = tf.keras.models.load_model('models/mnist_cnn_model/mnist_cnn_model.keras')
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Backend is running"}
+
+def preprocess_image(image_data: str) -> tf.Tensor:
+    # Remove the data URL prefix.
+    image_data = image_data.split(",")[1]
+
+    # Convert the base64 string to a numpy array and reshape it to the expected input shape for the model.
+    image_bytes = tf.io.decode_base64(image_data)
+
+    # Decode PNG to tensor. The 'channels=1' argument ensures that the image is treated as grayscale, which is appropriate for MNIST data.
+    image = tf.io.decode_png(image_bytes, channels=1)
+
+    # Downscale the image to the expected dimensions (uses bilinear interpolation by default) and add a channel dimension.
+    image = tf.image.resize(image, (IMAGE_HEIGHT, IMAGE_WIDTH))
+    
+    # Normalize the pixel values to the range [0, 1].
+    image = tf.cast(image, tf.float32) / MAX_PIXEL_VALUE
+
+    # Add a batch dimension to the image array to match the input shape expected by the model (1, 28, 28, 1).
+    image = tf.expand_dims(image, axis=0) 
+
+    return image
+
+@app.post("/predict")
+def predict(image: Image):
+
+    image = preprocess_image(image.data)
+    
+    # prediction will contain a list of probabilities for each digit, where each index corresponds to a digit (0-9).
+    prediction = model.predict(image)
+
+    # Get the index of the highest probability, which corresponds to its predicted digit.
+    most_likely_digit = tf.argmax(prediction, axis=1).numpy()[0]
+    predicted_digit = int(most_likely_digit)
+
+    return {"prediction": predicted_digit}
